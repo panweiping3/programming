c152f9409f50889ffaed9037fe58895896e60927
Peter Pan(潘卫平) <wppan@redflag-linux.com>
2010-04-30 16:46:51 +0800

    second release
    Signed-off-by: Peter Pan(潘卫平) <wppan@redflag-linux.com>

--- APUE/network/libsb/sb_http.cpp
+++ APUE/network/libsb/sb_http.cpp
@@ -4,29 +4,31 @@
 #include <QtNetwork/QHttpResponseHeader>
 
 #include "sb_http.h"
+
 SBHttp::SBHttp(QObject *parent) 
     : QObject(parent),
-      http_request_header(NULL),
-      downloadFile(NULL)
+      m_http_request_header(NULL),
+      m_downloadFile(NULL)
 {  
-    http_send_bytes.clear();
-    http_p = new QHttp(this);  
-    connect(http_p, SIGNAL(done(bool)), this, SLOT(done(bool)));
-    connect(http_p, SIGNAL(readyRead(const QHttpResponseHeader &)),
+    m_http_send_bytes.clear();
+    m_http_p = new QHttp(this);  
+    connect(m_http_p, SIGNAL(done(bool)), this, SLOT(done(bool)));
+    connect(m_http_p, SIGNAL(readyRead(const QHttpResponseHeader &)),
             this, SLOT(readAll(const QHttpResponseHeader &)));
 
-    connect(http_p, SIGNAL(requestFinished(int, bool)),
+    connect(m_http_p, SIGNAL(requestFinished(int, bool)),
             this, SLOT(on_http_requestFinished(int, bool)));
-    connect(http_p, SIGNAL(requestStarted(int)),
+    connect(m_http_p, SIGNAL(requestStarted(int)),
             this, SLOT(on_http_requestStarted(int)));
-    connect(http_p, SIGNAL(responseHeaderReceived(const QHttpResponseHeader &)),
+    connect(m_http_p, SIGNAL(responseHeaderReceived(const QHttpResponseHeader &)),
             this, SLOT(on_http_responseHeaderReceived(const QHttpResponseHeader &)));
-    http_request_header = new QHttpRequestHeader();
+    m_http_request_header = new QHttpRequestHeader();
 }
 
 SBHttp::~SBHttp()
 {
-    delete http_p;
+    delete m_http_p;
+    delete m_http_request_header;
 }
 
 void SBHttp::on_http_requestStarted(int id)
@@ -46,97 +48,62 @@ void SBHttp::on_http_requestFinished(int id, bool error)
 
     if (error) {
         qDebug() << "with errors";
-        qDebug() << http_p->errorString();
+        qDebug() << m_http_p->errorString();
     } else {
         qDebug() << "successfully id: " << id;
-        if (downloadFile) {
-            downloadFile->close();
+        if (m_downloadFile) {
+            m_downloadFile->close();
         }
     }
 }
 
-void SBHttp::post(const QUrl &url, const QString &file)
-{
-#if 0
-    QFile qFile(file);
-    qDebug() << qFile.fileName();
-    qFile.open( QIODevice::ReadOnly );
-
-    qDebug() << "host: " << url.host() << ", path: "<< url.path() << ", port:" << url.port();
-
-    QHttpRequestHeader header("POST", url.path());
-    header.setValue("Host", url.host());
-    header.setValue("Content-type", "multipart/form-data, boundary=AaB03x");
-    header.setValue("Cache-Control", "no-cache");
-    header.setValue("Accept","*/*");
-
-    QByteArray byt(file.toUtf8());
-    QByteArray bytes;
-    bytes.append("--AaB03x\r\n");
-    bytes.append("content-disposition: ");
-    // cupfile is a keyword in up.html
-    bytes.append("form-data; name=\"cupfile\"; filename=\"" + byt+ "\"\r\n");
-    bytes.append("Content-Transfer-Encoding: binary\r\n");
-    bytes.append("\r\n");
-    bytes.append(qFile.readAll());
-
-    qFile.close(); // the file is opened earlier in the code
-    bytes.append("\r\n");
-    bytes.append("--AaB03x--");
-    int contentLength = bytes.length();
-    header.setContentLength(contentLength);
-    http_p->setHost(url.host(), 80);
-    http_p->request(header, bytes);
-#endif
-}
-
 void SBHttp::post_start(const QUrl &url, const QString &fileName, int port)
 {
-    http_p->setHost(url.host(), port);
+    m_http_p->setHost(url.host(), port);
 
-    http_request_header->setRequest("POST", url.path(), 1, 1);
-    http_request_header->setValue("Host", url.host());
-    http_request_header->setValue("Content-type", "multipart/form-data, boundary=AaB03x");
-    http_request_header->setValue("Cache-Control", "no-cache");
-    http_request_header->setValue("Accept","*/*");
+    m_http_request_header->setRequest("POST", url.path(), 1, 1);
+    m_http_request_header->setValue("Host", url.host());
+    m_http_request_header->setValue("Content-type", "multipart/form-data, boundary=AaB03x");
+    m_http_request_header->setValue("Cache-Control", "no-cache");
+    m_http_request_header->setValue("Accept","*/*");
 
     QByteArray name(fileName.toUtf8());
-    http_send_bytes.append("--AaB03x\r\n");
-    http_send_bytes.append("content-disposition: ");
+    m_http_send_bytes.append("--AaB03x\r\n");
+    m_http_send_bytes.append("content-disposition: ");
     // cupfile is a keyword in up.html
-    http_send_bytes.append("form-data; name=\"cupfile\"; filename=\"" + name + "\"\r\n");
-    http_send_bytes.append("Content-Transfer-Encoding: binary\r\n");
-    http_send_bytes.append("\r\n");
+    m_http_send_bytes.append("form-data; name=\"cupfile\"; filename=\"" + name + "\"\r\n");
+    m_http_send_bytes.append("Content-Transfer-Encoding: binary\r\n");
+    m_http_send_bytes.append("\r\n");
 }
 
 void SBHttp::post_end(void) 
 {
-    http_send_bytes.append("\r\n");
-    http_send_bytes.append("--AaB03x--");
-    int len = http_send_bytes.length();
-    http_request_header->setContentLength(len);
-    http_p->request(*http_request_header, http_send_bytes);
+    m_http_send_bytes.append("\r\n");
+    m_http_send_bytes.append("--AaB03x--");
+    int len = m_http_send_bytes.length();
+    m_http_request_header->setContentLength(len);
+    m_http_p->request(*m_http_request_header, m_http_send_bytes);
 }
 
 void SBHttp::post_data(const char *buf, quint64 size)
 {
-    http_send_bytes.append(buf, size);
+    m_http_send_bytes.append(buf, size);
 }
 
 void SBHttp::get(const QUrl &url, const QString &file)  
 {  
-    downloadFile = new QFile(file);
-    downloadFile->open(QIODevice::ReadOnly);
+    m_downloadFile = new QFile(file);
+    m_downloadFile->open(QIODevice::ReadOnly);
 
-    http_p->setHost(url.host(), 80);
+    m_http_p->setHost(url.host(), 80);
     qDebug() << url.path();
-    http_p->get(url.path(), downloadFile);
-    http_p->close();
+    m_http_p->get(url.path(), m_downloadFile);
+    m_http_p->close();
 }
   
 void SBHttp::readAll(const QHttpResponseHeader &qrsh)
 {
-    QString sl = http_p->readAll();
+    QString sl = m_http_p->readAll();
     qDebug() << "SBHttp::readAll qrsh.toString : ";
     qDebug() << qrsh.toString();
     qDebug() << "--------------------------";
@@ -146,7 +113,7 @@ void SBHttp::done(bool error)
 {
     qDebug() << "SBHttp::done =============================";
     if(error) {
-        qDebug() << "Error: " << qPrintable(http_p->errorString()) << endl;
+        qDebug() << "Error: " << qPrintable(m_http_p->errorString()) << endl;
     }
 
     emit finished();
--- APUE/network/libsb/sb_http.h
+++ APUE/network/libsb/sb_http.h
@@ -12,41 +12,40 @@ class QHttpResponseHeader;
 
 class SBHttp : public QObject
 {
-Q_OBJECT
+    Q_OBJECT
 
-public:
-    SBHttp (QObject *parent = 0);
-    SBHttp (const QUrl url, int port, QObject *parent = 0);
-    ~SBHttp();
-    void setUrl(const QUrl &url) { m_url = url; }
-    QUrl &getUrl(void) { return m_url; }
-    void setPort(int port) { m_port = port; }
-    int getPort(void) { return m_port; }
-    void get(const QUrl &url, const QString &file);
-    void post(const QUrl &url, const QString &file);
-    void post_start(const QUrl &url, const QString &fileName, int port = 80);
-    void post_end(void);
-    void post_data(const char *buf, quint64 size);
+    public:
+        SBHttp (QObject *parent = 0);
+        SBHttp (const QUrl url, int port, QObject *parent = 0);
+        ~SBHttp();
+        void setUrl(const QUrl &url) { m_url = url; }
+        QUrl &getUrl(void) { return m_url; }
+        void setPort(int port) { m_port = port; }
+        int getPort(void) { return m_port; }
+        void get(const QUrl &url, const QString &file);
+        void post_start(const QUrl &url, const QString &fileName, int port = 80);
+        void post_end(void);
+        void post_data(const char *buf, quint64 size);
 
-public slots:
-    void on_http_requestFinished(int id, bool error);
-    void on_http_requestStarted(int id);
-    void on_http_responseHeaderReceived(const QHttpResponseHeader &resp);
+    public slots:
+        void on_http_requestFinished(int id, bool error);
+        void on_http_requestStarted(int id);
+        void on_http_responseHeaderReceived(const QHttpResponseHeader &resp);
 
-signals:
-    void finished();
+    signals:
+        void finished();
 
-private slots:
-    void done(bool error);
-    void readAll(const QHttpResponseHeader &);
+    private slots:
+        void done(bool error);
+        void readAll(const QHttpResponseHeader &);
 
-private:
-    QHttp *http_p;
-    QUrl m_url;
-    int m_port;
-    QHttpRequestHeader *http_request_header;
-    QFile *downloadFile;
-    QByteArray http_send_bytes;
+    private:
+        QHttp *m_http_p;
+        int m_port;
+        QUrl m_url;
+        QHttpRequestHeader *m_http_request_header;
+        QByteArray m_http_send_bytes;
+        QFile *m_downloadFile;
 };
 
 #endif
