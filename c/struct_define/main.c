c4ff03558557f00fae9ac44c4fd12f73a957b716
Weiping Pan <panweiping3@gmail.com>
2011-06-07 13:47:03 +0800

    iiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiidddddddddddddddddddddd Signed-off-by: Weiping Pan <panweiping3@gmail.com>

--- c/struct_define/main.c
+++ c/struct_define/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 struct HH{
-    #define A a 
+    #define A 10
     int a;
 };
 
@@ -9,8 +9,9 @@ int main()
 {
   struct HH h;
   h.a=19;
-
+	if (h.a > A) {
+		fprintf(stderr, "%s %d %s big\n", __FILE__, __LINE__, __func__);
+	}
   fprintf(stderr, "%s %d %s h.a=%d\n", __FILE__, __LINE__, __func__, h.a); 
-  fprintf(stderr, "%s %d %s h.A=%d\n", __FILE__, __LINE__, __func__, h.A); 
-//  fprintf(stderr, "%s %d %s A=%d\n", __FILE__, __LINE__, __func__, A); 
+//  fprintf(stderr, "%s %d %s h.A=%d\n", __FILE__, __LINE__, __func__, h.A); 
 }
