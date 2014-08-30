387bc4577af9bad82c36a5292266d614ee5fc137
Weiping Pan <panweiping3@gmail.com>
2014-08-30 10:25:14 +0800

    add process_fork
    Signed-off-by: Weiping Pan <panweiping3@gmail.com>

---
+++ APUE/process/process_fork/fork_zombie.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+
+int main()
+{
+	pid_t pid;
+	pid = fork();
+	if (pid < 0) {
+		perror("fork error:");
+		exit(1);
+	} else if (pid == 0) {
+		printf("I am child process.I am exiting.\n");
+		exit(0);
+	}
+	printf("I am father process.I will sleep 4 seconds\n");
+	//等待子进程先退出
+	sleep(4);
+	//输出进程信息
+	system("ps -o pid,ppid,state,tty,command");
+	printf("father process is exiting.\n");
+	return 0;
+}
