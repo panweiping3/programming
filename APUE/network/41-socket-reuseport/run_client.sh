0ca8962547c6b6fbfa808dd6778dea076be5ea78
Weiping Pan <panweiping3@gmail.com>
2014-09-07 16:57:38 +0800

    modify 41-socket-reuseport
    Signed-off-by: Weiping Pan <panweiping3@gmail.com>

--- APUE/network/41-socket-reuseport/run_client.sh
+++ APUE/network/41-socket-reuseport/run_client.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-for i in `seq 1000`
+for i in `seq $1`
 do 
 	./client &
 done
--- APUE/network/41-socket-reuseport/server.c
+++ APUE/network/41-socket-reuseport/server.c
@@ -7,16 +7,22 @@
 #include <string.h>
 #include <pthread.h>
 #include <time.h>
+#include <signal.h>
 #include <arpa/inet.h>
 #include <sys/syscall.h>
 
 #define SERVER_PORT 20000 		// define the defualt connect port id
 #define LENGTH_OF_LISTEN_QUEUE 10 	// length of listen queue in server
 #define BUFFER_SIZE 255
+#define THREAD_NUM 10
+
+pthread_t thread_array[THREAD_NUM];
+int thread_hit[THREAD_NUM];
 
 void *thread(void *data)
 {
-	pid_t tid = (pid_t)syscall(SYS_gettid);
+	pthread_t tid = pthread_self();
+
 	char buf[BUFFER_SIZE] = {'\0'};
 
 	int server_fd;
@@ -39,9 +45,10 @@ void *thread(void *data)
 	while (1) {
 		socklen_t length = sizeof(client_addr);
 		int client_fd = -1;
+		int i;
 
 		client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &length);
-		printf("thread %d a client connected: IP %s PORT %d\n",
+		printf("thread %lu a client connected: IP %s PORT %d\n",
 				tid,
 				inet_ntoa(client_addr.sin_addr),
 				ntohs(client_addr.sin_port));	
@@ -51,30 +58,44 @@ void *thread(void *data)
 
 			int ret = recv(client_fd, buf, BUFFER_SIZE, 0);
 			buf[ret-1] = '\0';
-			fprintf(stderr, "thread %d get data %s\n", tid, buf);
+			fprintf(stderr, "thread %lu get data %s\n", tid, buf);
 			close(client_fd);
 		}
+
+		for (i=0; i< THREAD_NUM; ++i)
+			if (thread_array[i] == tid)
+				thread_hit[i]++;
 	}
 
 	close(server_fd);
 	return NULL;
 }
 
+void haha()
+{
+	int i;
+	for (i=0; i<THREAD_NUM; ++i)
+		printf("thread %lu hit %d\n", thread_array[i], thread_hit[i]);
+	exit(0);
+}
+
 int main(int argc, char **argv)
 {
 	int i;
-	pthread_t thread_array[10];
 	pthread_t thread_id;
 
-	for (i=0; i<10; ++i) {
+	signal(SIGINT, haha);
+
+	for (i=0; i<THREAD_NUM; ++i) {
 		if (pthread_create(&thread_id, NULL, thread, NULL) < 0) {
 			fprintf(stderr, "create thread error\n");
 			exit(1);
 		}
 		thread_array[i] = thread_id;
+		thread_hit[i] = 0;
 	}
 
-	for (i=0; i<10; ++i) {
+	for (i=0; i<THREAD_NUM; ++i) {
 		pthread_join(thread_array[i], NULL);
 	}
 	return 0;
