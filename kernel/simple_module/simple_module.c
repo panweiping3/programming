3751c91863fe45406268ad086a8859c873b4f551
Weiping Pan <panweiping3@gmail.com>
2014-09-28 15:17:41 +0800

    add tcp_state_machine
    Signed-off-by: Weiping Pan <panweiping3@gmail.com>

--- kernel/simple_module/simple_module.c
+++ kernel/simple_module/simple_module.c
@@ -1,16 +1,18 @@
 #include <linux/init.h>  
 #include <linux/module.h>  
+#include <linux/log2.h>
 MODULE_LICENSE("GPL");  
 
 static __init int simple_module_init(void)  
 {  
-    printk(KERN_ALERT "module init!/n");  
+    printk(KERN_ALERT "module init!\n");  
+    printk(KERN_ALERT "ilog2(120)=%d\n", ilog2(120));  
     return 0;  
 }  
   
 static __exit void simple_module_exit(void)  
 {  
-    printk(KERN_ALERT "module exit!/n");  
+    printk(KERN_ALERT "module exit!\n");  
 }  
   
 module_init(simple_module_init);  
