4013ca6fae17a9fff373c1960ae8563dd83b891b
Weiping Pan <panweiping3@gmail.com>
2014-11-23 22:03:25 +0800

    modify namespace
    Signed-off-by: Weiping Pan <panweiping3@gmail.com>

--- APUE/system/namespace/ns_run.c
+++ APUE/system/namespace/ns_run.c
@@ -1,88 +1,35 @@
-/* simple_init.c
+/* ns_run.c
 
    Copyright 2013, Michael Kerrisk
    Licensed under GNU General Public License v2 or later
 
-   A simple init(1)-style program to be used as the init program in
-   a PID namespace. The program reaps the status of its children and
-   provides a simple shell facility for executing commands.
+   Join one or more namespaces using setns() and execute a command in
+   those namespaces, possibly inside a child process.
+
+   This program is similar in concept to nsenter(1), but has a
+   different command-line interface.
 */
 #define _GNU_SOURCE
+#include <fcntl.h>
+#include <sched.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <signal.h>
-#include <wordexp.h>
-#include <errno.h>
+#include <stdio.h>
 #include <sys/wait.h>
+
+/* A simple error-handling function: print an error message based
+   on the value in 'errno' and terminate the calling process */
 
 #define errExit(msg)    do { perror(msg); exit(EXIT_FAILURE); \
                         } while (0)
 
-static int verbose = 0;
-
-/* Display wait status (from waitpid() or similar) given in 'status' */
-
-/* SIGCHLD handler: reap child processes as they change state */
-
-static void
-child_handler(int sig)
-{
-    pid_t pid;
-    int status;
-
-    /* WUNTRACED and WCONTINUED allow waitpid() to catch stopped and
-       continued children (in addition to terminated children) */
-
-    while ((pid = waitpid(-1, &status,
-                          WNOHANG | WUNTRACED | WCONTINUED)) != 0) {
-        if (pid == -1) {
-            if (errno == ECHILD)        /* No more children */
-                break;
-            else
-                perror("waitpid");      /* Unexpected error */
-        }
-
-        if (verbose)
-            printf("\tinit: SIGCHLD handler: PID %ld terminated\n",
-                    (long) pid);
-    }
-}
-
-/* Perform word expansion on string in 'cmd', allocating and
-   returning a vector of words on success or NULL on failure */
-
-static char **
-expand_words(char *cmd)
-{
-    char **arg_vec;
-    int s;
-    wordexp_t pwordexp;
-
-    s = wordexp(cmd, &pwordexp, 0);
-    if (s != 0) {
-        fprintf(stderr, "Word expansion failed\n");
-        return NULL;
-    }
-
-    arg_vec = calloc(pwordexp.we_wordc + 1, sizeof(char *));
-    if (arg_vec == NULL)
-        errExit("calloc");
-
-    for (s = 0; s < pwordexp.we_wordc; s++)
-        arg_vec[s] = pwordexp.we_wordv[s];
-
-    arg_vec[pwordexp.we_wordc] = NULL;
-
-    return arg_vec;
-}
-
 static void
 usage(char *pname)
 {
-    fprintf(stderr, "Usage: %s [-q]\n", pname);
-    fprintf(stderr, "\t-v\tProvide verbose logging\n");
+    fprintf(stderr, "Usage: %s [-f] [-n /proc/PID/ns/FILE] cmd [arg...]\n",
+            pname);
+    fprintf(stderr, "\t-f     Execute command in child process\n");
+    fprintf(stderr, "\t-n     Join specified namespace\n");
 
     exit(EXIT_FAILURE);
 }
@@ -90,100 +37,61 @@ usage(char *pname)
 int
 main(int argc, char *argv[])
 {
-    struct sigaction sa;
-#define CMD_SIZE 10000
-    char cmd[CMD_SIZE];
+    int fd, opt, do_fork;
     pid_t pid;
-    int opt;
 
-    while ((opt = getopt(argc, argv, "v")) != -1) {
+    /* Parse command-line options. The initial '+' character in
+       the final getopt() argument prevents GNU-style permutation
+       of command-line options. That's useful, since sometimes
+       the 'command' to be executed by this program itself
+       has command-line options. We don't want getopt() to treat
+       those as options to this program. */
+
+    do_fork = 0;
+    while ((opt = getopt(argc, argv, "+fn:")) != -1) {
         switch (opt) {
-        case 'v': verbose = 1;          break;
-        default:  usage(argv[0]);
+
+        case 'n':       /* Join a namespace */
+            fd = open(optarg, O_RDONLY); /* Get descriptor for namespace */
+            if (fd == -1)
+                errExit("open");
+
+            if (setns(fd, 0) == -1)      /* Join that namespace */
+                errExit("setns");
+            break;
+
+        case 'f':
+            do_fork = 1;
+            break;
+
+        default:
+            usage(argv[0]);
         }
     }
 
-    sa.sa_flags = SA_RESTART | SA_NOCLDSTOP;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_handler = child_handler;
-    if (sigaction(SIGCHLD, &sa, NULL) == -1)
-        errExit("sigaction");
+    if (argc <= optind)
+        usage(argv[0]);
 
-    if (verbose)
-        printf("\tinit: my PID is %ld\n", (long) getpid());
+    /* If the "-f" option was specified, execute the supplied command
+       in a child process. This is mainly useful when working with PID
+       namespaces, since setns() to a PID namespace only places
+       (subsequently created) child processes in the names, and
+       does not affect the PID namespace membership of the caller. */
 
-    /* Performing terminal operations while not being the foreground
-       process group for the terminal generates a SIGTTOU that stops the
-       process.  However our init "shell" needs to be able to perform
-       such operations (just like a normal shell), so we ignore that
-       signal, which allows the operations to proceed successfully. */
-
-    signal(SIGTTOU, SIG_IGN);
-
-    /* Become leader of a new process group and make that process
-       group the foreground process group for the terminal */
-
-    if (setpgid(0, 0) == -1)
-        errExit("setpgid");;
-    if (tcsetpgrp(STDIN_FILENO, getpgrp()) == -1)
-        errExit("tcsetpgrp-child");
-
-    while (1) {
-
-        /* Read a shell command; exit on end of file */
-
-        printf("init$ ");
-        if (fgets(cmd, CMD_SIZE, stdin) == NULL) {
-            if (verbose)
-                printf("\tinit: exiting");
-            printf("\n");
-            exit(EXIT_SUCCESS);
-        }
-
-        if (cmd[strlen(cmd) - 1] == '\n')
-            cmd[strlen(cmd) - 1] = '\0';        /* Strip trailing '\n' */
-
-        if (strlen(cmd) == 0)
-            continue;           /* Ignore empty commands */
-
-        pid = fork();           /* Create child process */
+    if (do_fork) {
+        pid = fork();
         if (pid == -1)
             errExit("fork");
 
-        if (pid == 0) {         /* Child */
-            char **arg_vec;
-
-            arg_vec = expand_words(cmd);
-            if (arg_vec == NULL)        /* Word expansion failed */
-                continue;
-
-            /* Make child the leader of a new process group and
-               make that process group the foreground process
-               group for the terminal */
-
-            if (setpgid(0, 0) == -1)
-                errExit("setpgid");;
-            if (tcsetpgrp(STDIN_FILENO, getpgrp()) == -1)
-                errExit("tcsetpgrp-child");
-
-            /* Child executes shell command and terminates */
-
-            execvp(arg_vec[0], arg_vec);
-            errExit("execvp");          /* Only reached if execvp() fails */
+        if (pid != 0) {                 /* Parent */
+            if (waitpid(-1, NULL, 0) == -1)     /* Wait for child */
+                errExit("waitpid");
+            exit(EXIT_SUCCESS);
         }
 
-        /* Parent falls through to here */
-
-        if (verbose)
-            printf("\tinit: created child %ld\n", (long) pid);
-
-        pause();                /* Will be interrupted by signal handler */
-
-        /* After child changes state, ensure that the 'init' program
-           is the foreground process group for the terminal */
-
-        if (tcsetpgrp(STDIN_FILENO, getpgrp()) == -1)
-            errExit("tcsetpgrp-parent");
+        /* Child falls through to code below */
     }
-}
 
+    execvp(argv[optind], &argv[optind]);
+    errExit("execvp");
+}
